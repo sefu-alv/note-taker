64805f91ebf562bb702e0bc6279498feaf07b4ec
sefufim <132181028+sefufim@users.noreply.github.com>
2023-10-10 00:26:33 -0700

    added the POST  request and got api/notes to work

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,16 @@
 // requiring express
-const express = require("express");
+const express = require('express');
 
-const fs = require("fs");
+const fs = require('fs');
 
 const noteData = require('./db/db.json');
 
 const path = require("path");
 
 const port = process.env.port || 3001;
+
+// This package allows you to add an id to an item
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -24,8 +27,22 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('api/notes', (req, res) => res.json(noteData));
+// this route allows you to see the notes that have been saved
+app.get('/api/notes', (req, res) => res.json(noteData));
 
+// posting a new note
+app.post('/api/notes', (req, res) => {
+
+  const note = req.body;
+
+  note.id = uuidv4();
+
+  noteData.push(note);
+  
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
+
+  res.json(note);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
